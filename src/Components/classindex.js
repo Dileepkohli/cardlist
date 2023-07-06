b2604380687dd9963eed1dbeb8b6e8511862fc3d
dileep kumar <teludileepkumar@gmail.com>
2023-07-06 21:44:39 +0530

    Initial commit

--- src/Components/classindex.js
+++ src/Components/classindex.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
-import Cards from './cards';
-import Todolist from './todolist'
+
 import Cardlist from './cardlist';
-import Itemlist from './itemlist';
-import Dummy from './dummy';
+
 import './classindex.css'
 
 export default class classindex extends Component {
   render() {
     return (
       <div className='p-2'>
-         <Todolist/> 
-        {/* <Cardlist/><hr/> */}
-        {/* <Itemlist/> */}
-        <Dummy/> 
-        
+        <Cardlist/>
       </div>
     )
   }
--- src/Components/dummycard.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class dummycard extends Component {
-  render() {
-    return (
-      <div>dummycard</div>
-    )
-  }
-}
